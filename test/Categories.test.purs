8ff51525b50ed0eae18caec10d099f231936bdd8
David Komer <david.komer@gmail.com>
2018-08-23 00:07:38 +0300

    install quickcheck-laws

--- test/Categories.test.purs
+++ test/Categories.test.purs
@@ -2,13 +2,18 @@ module Test.Categories (testCategories) where
 
 import Prelude
 
+import SodiumFRP.Class(Stream, Cell, newStream)
 
 import Effect (Effect)
+import Effect.Class (liftEffect)
 import Test.Unit (suite, test)
 import Test.Unit.Main (runTest)
 
 import Test.Unit.QuickCheck (quickCheck)
+import Test.QuickCheck.Laws (A, B, C, checkLaws)
 import Test.QuickCheck (Result(), (===))
+import Test.QuickCheck.Laws.Data.Functor (checkFunctor)
+import Type.Proxy (Proxy2 (..))
 
 theCommutativeProperty :: Int -> Int -> Result
 theCommutativeProperty a b = (a + b) === (b + a)
@@ -17,4 +22,5 @@ testCategories :: Effect Unit
 testCategories = runTest do
     suite "[categories]" do
         test "[stream] functor" do
-            quickCheck theCommutativeProperty
+           liftEffect $ checkFunctor prxStream 
+        where prxStream = Proxy2 :: Proxy2 (Cell)
