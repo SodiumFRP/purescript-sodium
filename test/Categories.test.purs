bad805c27eee2e887bc5c3d568c662cdd7636125
David Komer <david.komer@gmail.com>
2018-08-23 23:19:12 +0300

    functor test

--- test/Categories.test.purs
+++ test/Categories.test.purs
@@ -2,25 +2,38 @@ module Test.Categories (testCategories) where
 
 import Prelude
 
-import SodiumFRP.Class(Stream, Cell, newStream)
-
+import SodiumFRP.Class(Cell, newCell)
+import SodiumFRP.Cell (sample)
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import Test.Unit (suite, test)
 import Test.Unit.Main (runTest)
-
-import Test.Unit.QuickCheck (quickCheck)
-import Test.QuickCheck.Laws (A, B, C, checkLaws)
-import Test.QuickCheck (Result(), (===))
 import Test.QuickCheck.Laws.Data.Functor (checkFunctor)
 import Type.Proxy (Proxy2 (..))
+import Test.QuickCheck.Arbitrary(class Arbitrary, arbitrary)
 
-theCommutativeProperty :: Int -> Int -> Result
-theCommutativeProperty a b = (a + b) === (b + a)
+
 
 testCategories :: Effect Unit
 testCategories = runTest do
     suite "[categories]" do
-        test "[stream] functor" do
-           liftEffect $ checkFunctor prxStream 
-        where prxStream = Proxy2 :: Proxy2 (Cell)
+        test "[cell] functor" do
+           liftEffect $ checkFunctor prxCell
+
+-- newtype wrapper so Arbitrary stuff isn't needed in main class
+newtype ArbitraryCell a = ArbitraryCell (Cell a)
+
+prxCell :: Proxy2 ArbitraryCell
+prxCell = Proxy2
+
+instance arbCell :: (Arbitrary a) => Arbitrary (ArbitraryCell a) where
+  arbitrary = do
+    a <- arbitrary
+    pure $ ArbitraryCell (newCell a) 
+
+instance eqArbitraryCell :: (Eq a) => Eq (ArbitraryCell a) where
+    eq (ArbitraryCell cell1) (ArbitraryCell cell2) = eq (sample cell1) (sample cell2)
+
+instance functorArbitraryCell :: Functor ArbitraryCell where
+    map func (ArbitraryCell a) = ArbitraryCell $ map func a
+
