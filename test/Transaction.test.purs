6e9d114f8a579d476dd377f60ac61bfe01bb777b
Clinton Selke <clinuxrulz@gmail.com>
2018-08-27 21:06:31 +1000

    loop1, loop2, loop3

--- test/Transaction.test.purs
+++ test/Transaction.test.purs
@@ -3,9 +3,9 @@ module Test.Transaction (testTransaction) where
 import Prelude
 
 import SodiumFRP.Transaction (runTransaction)
-import SodiumFRP.Class (newCellLoop, newCell, newStream, newStreamLoop)
+import SodiumFRP.Class (newCellLoop, newCell, newStream, newStreamLoop, loop2)
 import SodiumFRP.Cell (loopCell, sample)
-import SodiumFRP.Stream (loopStream) 
+import SodiumFRP.Stream (loopStream)
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import Test.Unit (suite, test)
@@ -21,20 +21,31 @@ testTransaction = runTest do
             )
             Assert.equal result 2
 
+        test "cell stream loop" do
+            result <- liftEffect $ do
+                cc <- loop2 (\ret ca' cb' -> do
+                    let cc = (_ * 2) <$> cb'
+                    let cb = (_ * 2) <$> ca'
+                    let ca = newCell 2
+                    pure $ ret ca cb cc
+                )
+                pure $ sample cc
+            Assert.equal result 8
+
         test "cell loop in transaction" do
             result <- liftEffect $ runTransaction (do
                 l <- newCellLoop
                 let c = newCell 2
                 loopCell c l
-                pure $ sample l 
+                pure $ sample l
             )
             Assert.equal result 2
-        
+
         test "stream loop in transaction" do
             result <- liftEffect $ runTransaction (do
                 l <- newStreamLoop
-                let s = newStream 
+                let s = newStream
                 loopStream s l
-                pure unit 
+                pure unit
             )
             pure unit
